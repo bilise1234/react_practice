b02b44f2b4ee6dd22bd6de99d2896cb8ca3bfd2e
bm <bm@gmail.com>
2026-06-09 01:42:28 -0900

    Merge remote-tracking branch 'origin/feature/useref' into feature/useref

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,4 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Home from "./pages/Home";
-import Products from "./pages/Products";
-import Contact from "./pages/Contact";
-import CounterPage from "./pages/CounterPage";
-import RegisterPage from "./pages/RegisterPage";
+import ProductDetail from "./pages/ProductDetail";
+// ...
+<Route path="/products/:id" element={<ProductDetail />} />;
 
-function App() {
-  return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<Products />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/counter" element={<CounterPage />} />
-        <Route path="/register" element={<RegisterPage />} />
-      </Routes>
-    </BrowserRouter>
-  );
-}
-export default App;
